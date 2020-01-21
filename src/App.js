cc2f976ebb4ff8746f7681f4cfdb1c6781065422
LoesJanssen <loesjanssen89@gmail.com>
2020-01-21 16:07:00 +0100

    Compnents file added AwesomeAnimals, LikeCounter and Title components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Title from "./components/Title";
+import LikeCounter from "./components/LikeCounter";
+import AwesomeAnimals from "./components/AwesomeAnimals";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Haha Loeske kan het wel :p</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <main>
+          <Title />
+          <LikeCounter />
+          <AwesomeAnimals />
+        </main>
       </header>
-      <p>Maar goed, das natuurlijk ook logisch</p>
     </div>
   );
 }
